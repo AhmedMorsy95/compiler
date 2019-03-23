250324e78746da11badefe5557da516d81fc3ccf
AhmedMorsy95 <ahmed.morsy1995@gmail.com>
2019-03-23 14:28:08 +0200

    modified it to be singleton class

--- NodeGenerator.py
+++ NodeGenerator.py
@@ -5,10 +5,23 @@ Only one NodeGenerator object in program , this is the factory of nodes that giv
 id
 """
 class NodeGenerator:
+    instance = None # class variable shared by all objects
+
+    @staticmethod
+    def getInstance():
+        if NodeGenerator.instance == None:
+           return NodeGenerator()
+        else:
+            return NodeGenerator.instance
+
     def __init__(self):
-        self.id=0
-        # keeping all nodes just in case needed to reference any
-        self.all_nodes=[]
+        if NodeGenerator.instance != None:
+            raise Exception("singleton class")
+        else:
+            NodeGenerator.instance = self
+            self.id = 0
+            # keeping all nodes just in case needed to reference any
+            self.all_nodes = []
 
     def make_node(self, isStart= False, isFinish= False, isDead= False):
         """
@@ -31,14 +44,16 @@ class NodeGenerator:
 
 if __name__ == '__main__':
     # just messing around
-    ng = NodeGenerator()
-    n1 = ng.make_node()
-    n2 = ng.make_node()
-    n3 = ng.make_node()
-    # test having multiple destinations for one key
-    n2.add_destination_node("character" , n1.id)
-    n2.add_destination_node("character", n3.id)
-
-    print(n1.id , n2.id, n3.id)
-    print(n2.move_destinations)
-    print("all nodes", [x.id for x in ng.all_nodes] )
+    ng = NodeGenerator.getInstance()
+    n2 = NodeGenerator.getInstance()    print(n2.id)
+    n3 = NodeGenerator.getInstance()
+    # n1 = ng.make_node()
+    # n2 = ng.make_node()
+    # n3 = ng.make_node()
+    # # test having multiple destinations for one key
+    # n2.add_destination_node("character" , n1.id)
+    # n2.add_destination_node("character", n3.id)
+    #
+    # print(n1.id , n2.id, n3.id)
+    # print(n2.move_destinations)
+    # print("all nodes", [x.id for x in ng.all_nodes] )
