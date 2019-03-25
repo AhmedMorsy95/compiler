f2d5c488833499b1ecac8bb2db33cbf4018edb26
AhmedMorsy95 <ahmed.morsy1995@gmail.com>
2019-03-25 17:10:11 +0200

    implemented dfa

--- DFA.py
+++ DFA.py
@@ -1,3 +1,5 @@
+import queue
+from dfaState import state
 class DFA:
     def __init__(self):
         self.accept_states = []
@@ -7,4 +9,20 @@ class DFA:
 
 
 def nfa_to_dfa(nfa):
-    # get the start state
+    symbols_list = ['a','b','c','d','e','f']
+    start_node = nfa.start_node
+    start_state = state.getState(start_node.transition_nodes('@'))
+    transition_table ={}
+    queue_state = queue.Queue()
+    queue_state.put(start_state)
+    visited = [start_state]
+    while(not queue_state.empty()):
+        state_pop = queue_state.get()
+        for symbol in symbols_list:
+           s= state_pop.get_transition_state(symbol)
+           if not visited.count(s):
+              queue_state.put(s)
+              visited.append(s)
+           state_pop.add_destination(symbol,s)
+
+    return start_state
