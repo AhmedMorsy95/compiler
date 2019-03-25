1ef7e33bd1bf6a0110cddff6b4b616ec410e255e
AhmedMorsy95 <ahmed.morsy1995@gmail.com>
2019-03-25 17:11:13 +0200

    implemented some functions to facilitate building transition table between state

--- dfaState.py
+++ dfaState.py
@@ -23,7 +23,7 @@ class state:
         nodes = sorted(nodes,key=state.sortHelper)
         state.state_Nodes.append(nodes)
         state.state_instances.append(self)
-        self.destinations = {} # we add in it a pair (char,state number)
+        self.destinations = {} # we add in it a pair (char,state)
         self.id = state.id
         self.labels = {}
         self.accept_state = False
@@ -34,22 +34,61 @@ class state:
     def getNodeFromID(id):
         return state.state_instances[id]
 
-    def getStateNodes(self):
+    def get_state_nodes(self):
         return state.state_Nodes[self.id]
 
-    def addDestination(self,char,state_to):
+    def add_destination(self,char,state_to):
             # only 1 state_to for each char, so we dont need a list
             self.destinations[char] = state_to
 
     def get_transition_state(self,char):
-        return None
+        nodes = self.get_state_nodes()
+        all_nodes = set()
+        for i in nodes:
+            child_nodes = i.transition_nodes(char)
+            for j in child_nodes:
+                all_nodes.add(j)
+
+        my_nodes = []
+        for i in all_nodes:
+            my_nodes.append(i)
+
+        return state.getState(my_nodes)
 
     def isAccept(self):
+        nodes = self.get_state_nodes()
+
+        for i in nodes:
+            if i.isFinish:
+                return True
+
         return False
 
     def getLabels(self):
-        return None
+        lbls = []
+        nodes = self.get_state_nodes()
+        for i in nodes:
+            if i.name != "" and lbls.count(i.name) == False:
+                lbls.append(i.name)
 
+        return lbls
+    def dfs_test(self,cur,visited,path):
+        if cur.isAccept():
+            print(path)
+        if visited.count(cur):
+            return
+        else:
+            visited.append(cur)
+            for key, value in cur.destinations.items():
+                path.append(key)
+                self.dfs_test(value,visited,path)
+                path.pop()
+            visited.pop()
+
+    def dfs_state(self):
+        visited = []
+        path = []
+        self.dfs_test(self,visited,path)
 
 if __name__ == '__main__':
     x = NodeGenerator.getInstance()
