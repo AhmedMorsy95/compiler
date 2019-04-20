b6f0de95069249c4ca23825e84c8ef6dcd97bdd0
AhmedMorsy95 <ahmed.morsy1995@gmail.com>
2019-04-20 17:48:49 +0200

    .

--- Main.py
+++ Main.py
@@ -7,6 +7,5 @@ if __name__ == '__main__':
     file_name = "input.txt"
     procution_rules,start_symbol = Input.read_input(file_name)
     grammar_instance  = Grammar(procution_rules,start_symbol)
-    print(grammar_instance.is_terminal("IF"))
     # parser_instance = Parser(grammar_instance)
     # parser_instance.build()
