ba5ca1b1f0e00f2bcf83e55a94bd30b56dedd120
Louay Morsi <louay.hesham@gmail.com>
2019-04-24 11:08:50 +0200

    Made sure code only runs when main.py is specifically ran

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 from tokenizer.tokenizer import get_tokens
 
-tokens = get_tokens("grammar.txt", "input.txt", print_tokens=True)
+if __name__ == '__main__':
+    tokens = get_tokens("grammar.txt", "input.txt", print_tokens=True)
